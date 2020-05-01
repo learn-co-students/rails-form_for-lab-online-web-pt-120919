435458033b3e960b57a1a178bfde3cc6d5a655cc
Christian_Fredericksen <christian.fredericksen@yahoo.com>
2020-05-01 12:55:58 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,9 +4,9 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @students = Student.new(student_params)
-    @students.save
-    redirect_to student_path(@students)
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def index
@@ -14,13 +14,17 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(student_params)
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+		@student.update(params.require(:student).permit(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def destroy
